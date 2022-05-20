112ae415471649b3f43ca5d795253c367177ba1e
KrisUz <60334543+KrisUz@users.noreply.github.com>
2022-05-19 21:26:25 -0700

    add python file

---
+++ task.py
@@ -0,0 +1,8 @@
+hungry=input("are you hungry?")
+if hungry=="yes":
+	print("have some rice")
+	print("what about piza")
+	print("how about burger?")
+else:
+	task=inpu("are you done?"
+	print("the do your homework")
